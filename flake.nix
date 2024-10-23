2eec1197228abf081eff0326d21ac4159a415501
Luna Heyman <luna.heyman@proton.me>
2024-10-23 18:10:22 +0200

    chore: initialize nix flake

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  description = "My Nix and Mac OS configurations in a Nix flake.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = inputs: { };
+}
