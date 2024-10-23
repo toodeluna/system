a5832a13c3567a176eafad2dd0aeaffb0bd2919d
Luna Heyman <luna.heyman@proton.me>
2024-10-23 18:13:29 +0200

    feat: add formatter

--- flake.nix
+++ flake.nix
@@ -5,5 +5,14 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = inputs: { };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      pkgs = {
+        x86_64-linux = import nixpkgs { system = "x86_64-linux"; };
+      };
+    in
+    {
+      formatter.x86_64-linux = pkgs.x86_64-linux.nixfmt-rfc-style;
+    };
 }
