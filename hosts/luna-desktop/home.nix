983dcdcf6c2232c691ce9a3264d00c5dd1ffda3e
Luna Heyman <luna.heyman@proton.me>
2024-10-23 20:47:53 +0200

    feat: add git config to home-manager

--- hosts/luna-desktop/home.nix
+++ hosts/luna-desktop/home.nix
@@ -5,4 +5,18 @@
   home.homeDirectory = "/home/luna";
 
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Luna Heyman";
+    userEmail = "luna.heyman@proton.me";
+    aliases = {
+      lga = "log --graph --oneline --all --decorate";
+      put = "push --set-upstream";
+      rollback = "reset --hard HEAD";
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
 }
