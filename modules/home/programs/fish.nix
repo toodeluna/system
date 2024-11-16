29de52a093726d8398dd9f759a25e070deaff986
Luna Heyman <luna.heyman@proton.me>
2024-11-16 16:01:45 +0100

    feat: add fish vi key bindings

--- modules/home/programs/fish.nix
+++ modules/home/programs/fish.nix
@@ -3,6 +3,7 @@
   config = lib.mkIf systemConfiguration.syscfg.programs.fish.enable {
     programs.fish = {
       enable = true;
+      interactiveShellInit = "fish_vi_key_bindings";
     };
   };
 }
