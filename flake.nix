2886e13f2d93f8e137fb4e8064d8e213628d828a
Luna Heyman <luna.heyman@proton.me>
2024-12-04 16:32:07 +0100

    feat: initialize nix flake

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "My Linux and MacOS system configurations in a Nix flake.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = inputs: {
+  };
+}
